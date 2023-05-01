05799858931df860a75a7b209b57bf6fc0ab0485
Eugen1344 <eugen1344@gmail.com>
2023-05-02 01:09:44 +0300

    + score settings + unity serializable dictionary

---
+++ Assets/Scripts/GameScore/ScoreSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using UnityEngine;
+using Utils;
+
+namespace GameScore
+{
+    public class ScoreSettings : ScriptableObject
+    {
+        [SerializeField] private UnitySerializedDictionary<int, int> _scoreForMergeValue;
+
+        public int GetScoreForMerge(int mergeValue)
+        {
+            if (_scoreForMergeValue.TryGetValue(mergeValue, out int score))
+                return score;
+
+            return _scoreForMergeValue.Values.Last();
+        }
+    }
+}
